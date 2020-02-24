59911413114d3b992932d9adf51797d637f81d4e
pedroNahime <pcnahime12@gmail.com>
2020-02-24 17:09:11 +0000

    add tests for user reducers and actions

---
+++ src/actions/user/test/user.test.js
@@ -0,0 +1,18 @@
+import { Types, Creators } from '../index'
+
+describe('Types', () => {
+  it('should export the expected action types', () => {
+    expect(Types).toEqual({
+      SAVE_USER: '@user/SAVE_USER'
+    })
+  })
+})
+
+describe('Creators', () => {
+  it('should return the saveUser action', () => {
+    expect(Creators.saveUser({ user: 'user' })).toEqual({
+      type: Types.SAVE_USER,
+      payload: { user: { user: 'user' } }
+    })
+  })
+})
---
+++ src/reducers/user/test/user.test.js
@@ -0,0 +1,30 @@
+import { initialState, userReducer } from '../index'
+
+describe('initialState', () => {
+  test('should set the initial state ', () => {
+    expect(initialState).toEqual({
+      firstName: '',
+      lastName: '',
+      age: ''
+    })
+  })
+})
+
+describe('Reducers', () => {
+  describe('SAVE_USER', () => {
+    test('Should save user', () => {
+      const action = {
+        type: 'SAVE_USER',
+        payload: {
+          user: {
+            firstName: 'some_name'
+          }
+        }
+      }
+      expect(userReducer(initialState, action)).toEqual({
+        ...initialState,
+        ...action.user
+      })
+    })
+  })
+})
