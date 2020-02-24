41b2eb898228aa8ff804c8905ec415d118e9491f
João Barreira <joao.barreira@talkdesk.com>
2020-02-24 15:51:01 +0000

    Add mapDispatchToProps tests (User)

--- src/components/user/tests/user.test.js
+++ src/components/user/tests/user.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { User } from '../../user'
+import { mapDispatchToProps } from '../../user'
+import { Creators as UserCreators } from '../../../actions/user'
 
 describe('User component', () => {
   let wrapper
@@ -17,5 +19,19 @@ describe('User component', () => {
     const inputs = wrapper.find('Input')
 
     expect(inputs).toHaveLength(3)
+  })
+
+  describe('#mapDispatchToProps', () => {
+    let dispatch, props
+
+    beforeEach(() => {
+      dispatch = jest.fn()
+      props = mapDispatchToProps(dispatch)
+    })
+
+    it('should dispatch saveUser', () => {
+      props.saveUser()
+      expect(dispatch).toHaveBeenCalledWith(UserCreators.saveUser())
+    })
   })
 })
